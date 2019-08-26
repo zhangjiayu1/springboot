bad93e69b5f82c3daf4a00f082facd7a11d8f6cd
13439668817 <13439668817@163.com>
2019-08-26 14:17:30 +0800

    chapter 24

---
+++ springboot/chapter24/src/main/java/com/ztest/chapter24/Chapter24Application.java
@@ -0,0 +1,13 @@
+package com.ztest.chapter24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter24Application.class, args);
+    }
+
+}
