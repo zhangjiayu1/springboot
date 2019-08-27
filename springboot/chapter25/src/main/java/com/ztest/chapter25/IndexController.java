837607c99d9ff4f85b865b9ec574f4f692cf02d0
13439668817 <13439668817@163.com>
2019-08-27 14:49:44 +0800

    chapter 25 SpringBoot添加支持CORS跨域访问

---
+++ springboot/chapter25/src/main/java/com/ztest/chapter25/IndexController.java
@@ -0,0 +1,24 @@
+package com.ztest.chapter25;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* @author: 11568
+ * \* @date: 2019/08/27
+ * \* Time: 10:29
+ * \* 码云: https://gitee.com/SXQZ/springboot
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@RestController
+public class IndexController
+{
+
+    @RequestMapping(value = "/cors")
+    public String corsIndex(){
+        return "this is cors info.";
+    }
+}
---
+++ springboot/chapter25/src/main/java/com/ztest/chapter25/config/CORSConfiguration.java
@@ -0,0 +1,32 @@
+package com.ztest.chapter25.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* @author: 11568
+ * \* @date: 2019/08/27
+ * \* Time: 10:29
+ * \* 码云: https://gitee.com/SXQZ/springboot
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@Configuration
+public class CORSConfiguration extends WebMvcConfigurationSupport{
+
+    @Override
+    protected void addCorsMappings(CorsRegistry registry) {
+        registry
+                //配置可以被跨域的路径，可具体到直接请求路径
+                .addMapping("/**")
+                //允许所有请求方法放蚊该跨域资源服务器，也可设置固定某个
+                .allowedMethods("*")
+                //允许所有的请求域名访问跨域资源，也可固定单挑或者多条
+                .allowedOrigins("*")
+                //允许所有的请求header访问，可以自定义设置任意请求头信息
+                .allowedHeaders("*");
+    }
+}
