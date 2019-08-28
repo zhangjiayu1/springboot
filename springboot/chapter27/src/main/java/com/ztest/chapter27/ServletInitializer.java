a329e5878e05f989273478d0e2236a14ba62a0fe
13439668817 <13439668817@163.com>
2019-08-28 14:54:39 +0800

    chapter 27

---
+++ springboot/chapter27/src/main/java/com/ztest/chapter27/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ztest.chapter27;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Chapter27Application.class);
+    }
+
+}
