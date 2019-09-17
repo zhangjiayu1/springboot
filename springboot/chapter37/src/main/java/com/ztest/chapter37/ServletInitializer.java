0f1fa9b4aec3f0a0079b03b84d2e94962b978383
13439668817 <13439668817@163.com>
2019-09-17 15:56:37 +0800

    chapter 37

---
+++ springboot/chapter37/src/main/java/com/ztest/chapter37/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.ztest.chapter37;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Chapter37Application.class);
+    }
+
+}
